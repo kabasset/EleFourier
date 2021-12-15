adb4cf6df520d7f9083a325f30bc4da559584a13
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2021-12-15 15:49:24 +0100

    WIP: play with scipy and fft functions

---
+++ PyFFTPlayground/python/PyFFTPlayground/Timer.py
@@ -0,0 +1,58 @@
+#
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
+"""
+:file: python/PyFFTPlayground/Timer.py
+
+:date: 12/15/21
+:author: https://realpython.com/python-timer/
+
+"""
+import time
+
+
+class TimerError(Exception):
+    """A custom exception used to report errors in use of Timer class
+       Taken from https://realpython.com/python-timer/
+    """
+
+
+class Timer:
+    def __init__(self, text="Elapsed time: {:0.4f} seconds", logger=print):
+        self._start_time = None
+        self.text = text
+        self.logger = logger
+
+    def start(self):
+        """Start a new timer"""
+        if self._start_time is not None:
+            raise TimerError(f"Timer is running. Use .stop() to stop it")
+
+        self._start_time = time.perf_counter()
+
+    def stop(self):
+        """Stop the timer, and report the elapsed time"""
+        if self._start_time is None:
+            raise TimerError(f"Timer is not running. Use .start() to start it")
+
+        elapsed_time = time.perf_counter() - self._start_time
+        self._start_time = None
+        if self.logger:
+            self.logger(self.text.format(elapsed_time))
+
+        return elapsed_time
