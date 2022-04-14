8013d490f733fc8536e21ee2deee614403d8e5c0
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2022-04-14 17:49:22 +0200

    update script

--- PyFFTParallelProfiling.sh
+++ PyFFTParallelProfiling.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=PyFFTParallelProfiling_%j.log   # Standard output and error log
 
 #SBATCH --partition flash              # Partition choice
-#SBATCH --ntasks 20                    # Run a single task (by default tasks == CPU)
+#SBATCH --ntasks 40                    # Run a single task (by default tasks == CPU)
 
 #SBATCH --mail-user=manuel.grizonnet@cnes.fr  # Where to send mail
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -14,5 +14,5 @@
 source /cvmfs/euclid-dev.in2p3.fr/CentOS7/EDEN-3.0/bin/activate
 
 cd /pbs/home/g/grizonnm/Work/Projects/EleFourier/
-END=30
-for i in $(seq 1 $END); do time ./build.x86_64-conda_cos6-gcc93-o2g/run PyFFTParallel -l 40 -p 20 -b $i; done
+END=40
+for i in $(seq 1 $END); do time ./build.x86_64-conda_cos6-gcc93-o2g/run PyFFTParallel -l 40 -p 40 -b $i; done
